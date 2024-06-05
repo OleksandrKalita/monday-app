1e2df4fe3f0c47281981cf6d70bf962f0e194915
Oleksandr Kalita <olexadr.kalita@gmail.com>
2024-06-05 11:48:16 +0200

    add footer menu animation

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,3 +2,80 @@ import './home.scss';
 import '../../components/component';
 
 console.log('Home Page');
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const headlineFirst = document.getElementById("headline_1");
+
+//     if (headlineFirst) {
+//         headlineFirst.addEventListener("click", () => {
+//             console.log("toggle")
+//             headlineFirst.classList.toggle("active");
+//         });
+//     }
+// });
+
+window.onload = () => {
+    const headlineFirst = document.getElementById("headline_1");
+    const headlineSecond = document.getElementById("headline_2");
+    const headlineThird = document.getElementById("headline_3");
+    const headlineFourth = document.getElementById("headline_4");
+
+    if (headlineFirst && headlineSecond && headlineThird && headlineFourth) {
+        headlineFirst.addEventListener("click", () => {
+            console.log("toggle")
+            headlineFirst.classList.toggle("active");
+            
+            headlineSecond.classList.remove("active");
+            headlineThird.classList.remove("active");
+            headlineFourth.classList.remove("active");
+        });
+
+        headlineSecond.addEventListener("click", () => {
+            console.log("toggle")
+            headlineSecond.classList.toggle("active");
+
+            headlineFirst.classList.remove("active");
+            headlineThird.classList.remove("active");
+            headlineFourth.classList.remove("active");
+        });
+
+        headlineThird.addEventListener("click", () => {
+            console.log("toggle")
+            headlineThird.classList.toggle("active");
+
+            headlineFirst.classList.remove("active");
+            headlineSecond.classList.remove("active");
+            headlineFourth.classList.remove("active");
+        });
+
+        headlineFourth.addEventListener("click", () => {
+            console.log("toggle")
+            headlineFourth.classList.toggle("active");
+
+            headlineFirst.classList.remove("active");
+            headlineSecond.classList.remove("active");
+            headlineThird.classList.remove("active");
+        });
+    }  
+
+    // if (headlineSecond) {
+    //     headlineSecond.addEventListener("click", () => {
+    //         console.log("toggle")
+    //         headlineSecond.classList.toggle("active");
+    //     });
+
+    // } 
+    // if (headlineThird) {
+    //     headlineThird.addEventListener("click", () => {
+    //         console.log("toggle")
+    //         headlineThird.classList.toggle("active");
+    //     });
+    // } 
+    // if (headlineFourth) {
+    //     headlineFourth.addEventListener("click", () => {
+    //         console.log("toggle")
+    //         headlineFourth.classList.toggle("active");
+    //     });
+    // } 
+}
